2e7b7c3ce3ff84dc865c1b69225a7f26cb2e58b3
«VladimirD» <fotodrom@rambler.ru>
2023-02-12 22:07:44 +0300

    3

---
+++ our/№3/19/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+Console.WriteLine("Введите пятизначное число");
+//void number (int num);
+int num = Convert.ToInt32(Console.ReadLine());
+//double num1 = num/100000;
+//double num2 = num /10;
+//double num3 = num / 10000;
+//double num4 = num /100;
+
+// if ((Math.Round(num1 , 1)) = (Math.Round(num4 , 1))) && ((Math.Round(num2 , 1)) = (Math.Round(num3 , 1)))
+
+    //if
+
+      int num1 = num % 10;
+      //int num4 = num % 10;
+    
+    Console.WriteLine(num1);
+
+
+
+//else
+   // Console.WriteLine("Введенное число не является полиндромом");
---
+++ our/№3/21/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координаты Ax: ");
+double Ax = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Ay: ");
+double Ay = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Az: ");
+double Az = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Bx: ");
+double Bx = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты By: ");
+double By = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты Bz: ");
+double Bz = Convert.ToInt32(Console.ReadLine());
+
+double distanse = Math.Sqrt(Math.Pow(Bx - Ax, 2) + Math.Pow(By - Ay, 2) + Math.Pow(Bz - Az, 2));
+
+Console.WriteLine( $"Расстояние между точками составляет: {(Math.Round(distanse , 2))}");
