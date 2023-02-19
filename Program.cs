0b8f6ac9a4f4210b9108efe320103d1d0157dac6
«VladimirD» <fotodrom@rambler.ru>
2023-02-19 19:11:48 +0300

    задача 41

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int [] num = {-1,-7,567,89,223};
+Console.WriteLine(num);
+int sum = 0;
+for (int i = 0; i < 5; i++)
+{
+    if (num[i] >0)
+    sum++;
+}
+
+Console.WriteLine(string.Join(",",num));
+Console.WriteLine(sum);
