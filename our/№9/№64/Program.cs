305caadfd58600480607f6cd83acfe825b433915
VladimirDolmatov1978 <122998722+VladimirDolmatov1978@users.noreply.github.com>
2023-02-28 11:08:15 +0300

    Add files via upload

---
+++ our/№9/№64/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+int Prompt(string massege)
+{
+    System.Console.Write(massege);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+
+void GetNum (int num)
+{
+    if (num  == 0) return;
+     
+    {
+        Console.Write("{0,3}", num);
+
+    }
+    GetNum (num - 1);
+}  
+
+int n = Prompt("введи N: ");
+GetNum(n);
---
+++ our/№9/№66/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+int Prompt(string massege)
+{
+    System.Console.Write(massege);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+int SummNum(int min, int max)
+{
+    if (min == max)
+        return max;
+    else
+    {
+        return min + SummNum(min+1, max);
+    }
+}
+
+
+int m = Prompt("введи M: ");
+int n = Prompt("введи N: ");
+System.Console.WriteLine(SummNum(m, n));
